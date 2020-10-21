f5500feed0e636d124b2197712b699bf92584214
awataki <awataki1206@gmail.com>
2020-10-21 11:32:08 +0900

    add toDto

--- External/src/main/kotlin/sh/awtk/kamuhakari/exposed/table/UserTable.kt
+++ External/src/main/kotlin/sh/awtk/kamuhakari/exposed/table/UserTable.kt
@@ -6,6 +6,10 @@ import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.LongIdTable
 import org.jetbrains.exposed.sql.Column
 import org.joda.time.DateTime
+import sh.awtk.kamuhakari.dto.UserDto
+import sh.awtk.kamuhakari.vo.OneTimeURL
+import sh.awtk.kamuhakari.vo.RoomId
+import sh.awtk.kamuhakari.vo.UserId
 
 object UserTable : LongIdTable("User") {
     var roomId: Column<EntityID<Long>> = reference("room_id", RoomTable)
@@ -19,4 +23,13 @@ class UserEntity(id: EntityID<Long>) : LongEntity(id) {
     var roomId by RoomEntity referencedOn UserTable.roomId
     var oneTimeURL by UserTable.oneTimeURL
     var expiredAt by UserTable.expiredAt
+
+    fun toDto(): UserDto {
+        return UserDto(
+            UserId(id.value),
+            RoomId(roomId.rowId),
+            OneTimeURL(oneTimeURL),
+            expiredAt
+        )
+    }
 }
